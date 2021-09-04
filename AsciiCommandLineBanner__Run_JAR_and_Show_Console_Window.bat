a9428e0ee71d1cae59d73c86a69111b1905c1bb7
MooseValley <Moose_Software@yahoo.com.au>
2021-09-04 21:42:39 +1000

    v0.1
    Add better example to README

--- AsciiCommandLineBanner__Run_JAR_and_Show_Console_Window.bat
+++ AsciiCommandLineBanner__Run_JAR_and_Show_Console_Window.bat
@@ -1,4 +1,4 @@
 REM "%JAVA_HOME%\bin\java.exe" -jar AsciiCommandLineBanner.jar
 REM java.exe -jar AsciiCommandLineBanner.jar Moose Valley
-java.exe -jar AsciiCommandLineBanner.jar "Moose Software" "Valley - Est. Jul-1996"
+java.exe -jar AsciiCommandLineBanner.jar "Moose's Software" Valley "Est. Jul-1996"
 pause
